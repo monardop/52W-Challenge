31053c6aa9e03ca4eca58dee0f1858436038b73b
Pablo Monardo <pablomonardo@gmail.com>
2022-12-23 16:44:31 -0300

    Created week 1

---
+++ week1.py
@@ -0,0 +1,11 @@
+"""
+* ¿ES UN ANAGRAMA?
+ * Fecha publicación enunciado: 03/01/22
+ * Fecha publicación resolución: 10/01/22
+ * Dificultad: MEDIA
+ *
+ * Enunciado: Escribe una función que reciba dos palabras (String) y retorne verdadero o falso (Boolean) según sean o no anagramas.
+ * Un Anagrama consiste en formar una palabra reordenando TODAS las letras de otra palabra inicial.
+ * NO hace falta comprobar que ambas palabras existan.
+ * Dos palabras exactamente iguales no son anagrama.
+"""
