0de2cfaf1dbc2f48bc8ca244bdbb77dad1e27e17
Pablo Monardo <pablomonardo@gmail.com>
2022-12-23 17:03:37 -0300

    Create week2.py

---
+++ week2.py
@@ -0,0 +1,11 @@
+"""
+ * Enunciado: Escribe un programa que imprima los 50 primeros números de la sucesión de Fibonacci empezando en 0.
+ * La serie Fibonacci se compone por una sucesión de números en la que el siguiente siempre es la suma de los dos anteriores.
+ * 0, 1, 1, 2, 3, 5, 8, 13...
+"""
+
+fibonacci = [0,1]
+while len(fibonacci)!=50:
+    fibonacci.append(fibonacci[-1]+fibonacci[-2])
+
+print(fibonacci)
