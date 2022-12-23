943a575fa1379e6ac790a2623aca909a81605d6c
Pablo Monardo <pablomonardo@gmail.com>
2022-12-23 16:58:20 -0300

    Week2 done

--- week1.py
+++ week1.py
@@ -1,7 +1,5 @@
 """
 * ¿ES UN ANAGRAMA?
- * Fecha publicación enunciado: 03/01/22
- * Fecha publicación resolución: 10/01/22
  * Dificultad: MEDIA
  *
  * Enunciado: Escribe una función que reciba dos palabras (String) y retorne verdadero o falso (Boolean) según sean o no anagramas.
@@ -9,3 +7,22 @@
  * NO hace falta comprobar que ambas palabras existan.
  * Dos palabras exactamente iguales no son anagrama.
 """
+palabra1 = input("Ingrese primera palabra: ").lower()
+palabra2 = input("Ingrese segunda palabra: ").lower()
+
+if palabra1 == palabra2:
+    print("Ambas palabras son iguales")
+    exit(1)
+
+letter_count = {}
+for letter in palabra1:
+    if letter in letter_count:
+        letter_count[letter] += 1
+    else:
+        letter_count[letter] = 1
+
+for letter in letter_count.keys():
+    if palabra2.count(letter) != letter_count[letter]:
+        print("Son distintos")
+        exit(1)
+print("Son anagramas")
