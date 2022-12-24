a58a5a011ab65755770e19ad841c05d831007dca
Pablo Monardo <pablomonardo@gmail.com>
2022-12-24 17:16:48 -0300

    Update week3.py

--- Python/week3.py
+++ Python/week3.py
@@ -1,14 +1,17 @@
 """
  * ¿ES UN NÚMERO PRIMO?
  * Dificultad: MEDIA
- *
  * Enunciado: Escribe un programa que se encargue de comprobar si un número es o no primo.
  * Hecho esto, imprime los números primos entre 1 y 100.
  *
 """
+def prime_number(number: int) -> bool:
+    for n in range(2,number):
+        if number%n == 0:
+            return False
+    return True 
 
 def main():
-    print("Insertar un número, se darán 100 números con el suyo como centro")
     while True:
         num = input("Insertar un numero: ")
         try: 
@@ -17,6 +20,10 @@ def main():
             print("Eso no es un número.")
         else:
             break
+    if prime_number(num):
+        print("Es primo")
+    else:
+        print("No es primo")
 
 if __name__ == "__main__":
     main()
