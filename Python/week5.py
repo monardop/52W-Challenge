1735579a85fa02f6ffd26387f503ec1673560473
Pablo Monardo <pablomonardo@gmail.com>
2022-12-25 09:51:56 -0300

    Update week5.py

--- Python/week5.py
+++ Python/week5.py
@@ -12,6 +12,7 @@
 """
 
 import requests
+import cv2
 
 url_imagen = "https://raw.githubusercontent.com/mouredev/mouredev/master/mouredev_github_profile.png"
 
@@ -19,3 +20,18 @@ imagen = requests.get(url_imagen).content
 
 with open("imagen.jpg","wb") as f:
     f.write(imagen)
+
+img = cv2.imread("imagen.jpg")
+wid = img.shape[1]
+hgt = img.shape[0]
+
+ratio = round(wid/hgt, 2)
+
+if ratio == 1.78:
+    print("The ratio is 16:9")
+elif ratio == 1.33:
+    print("The ratio is 4:3")
+elif ratio == 1:
+    print("The ratio is 1:1 (square)")
+else: 
+    print("Unknow ratio")
