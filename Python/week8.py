e5f58907e4e306ebfa1c40e4308544fd0894ac85
Pablo Monardo <pablomonardo@gmail.com>
2022-12-27 15:33:18 -0300

    Create week8.py

---
+++ Python/week8.py
@@ -0,0 +1,9 @@
+"""
+/*
+ * Reto #8
+ * DECIMAL A BINARIO
+ * Dificultad: FÁCIL
+
+ * Enunciado: Crea un programa se encargue de transformar un número decimal a binario sin utilizar funciones propias
+   del lenguaje que lo hagan directamente.
+"""
