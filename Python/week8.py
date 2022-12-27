98cdac85fdca00edb8f0ed75bf17ae4233ff4007
Pablo Monardo <pablomonardo@gmail.com>
2022-12-27 16:02:20 -0300

    Finished

--- Python/week8.py
+++ Python/week8.py
@@ -7,3 +7,35 @@
  * Enunciado: Crea un programa se encargue de transformar un número decimal a binario sin utilizar funciones propias
    del lenguaje que lo hagan directamente.
 """
+
+
+def decimal_binary(number: int) -> list:
+    decimal = []
+    while True:
+        decimal.append(number % 2)
+        number //= 2
+        if number // 2 == 0:
+            decimal.append(1)
+            break
+    return decimal
+
+
+def get_number() -> int:
+    while True:
+        try:
+            number = int(input("Insert a integer number: "))
+        except ValueError:
+            print("Wrong input.")
+        else:
+            return number
+
+
+def main():
+    number: int = get_number()
+    binary: list = decimal_binary(number)
+    while len(binary) != 0:
+        print(binary.pop(), end="")
+
+
+if __name__ == '__main__':
+    main()
