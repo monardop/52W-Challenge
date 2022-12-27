7d992bdf9d59587a41b900b6fa80da7f814f2132
Pablo Monardo <pablomonardo@gmail.com>
2022-12-27 17:15:32 -0300

    Update week9.py

--- Python/week9.py
+++ Python/week9.py
@@ -10,3 +10,58 @@
  * - En morse se soporta raya "—", punto ".", un espacio " " entre letras o símbolos y dos espacios entre palabras "  ".
  * - El alfabeto morse soportado será el mostrado en https://es.wikipedia.org/wiki/Código_morse.
 """
+
+morse_code = {
+    'A': '· —',
+    'B': '— · · ·	',
+    'C': '	— · — ·',
+    'CH': '— — — —',
+    'D': '— · ·',
+    'E': '·',
+    'F': '· · — ·',
+    'G': '- - ·',
+    'H': '· · · ·',
+    'I': '· ·',
+    'J': '· - - -',
+    'K': '- · -',
+    'L': '· - · ·',
+    'M': '- -',
+    'N': '- ·',
+    'Ñ': '- - · - -',
+    'O': '- - -',
+    'P': '· - - ·',
+    'Q': '- - · -',
+    'R': '· - ·',
+    'S': '· · ·',
+    'T': '-',
+    'U': '· · -',
+    'V': '· · · -',
+    'W': '· - -',
+    'X': '- · · -',
+    'Y': '- · - - ',
+    'Z': '- - · ·',
+    '0': '- - -',
+    '1': '· - - - -',
+    '2': '· · - - -',
+    '3': '· · · - -',
+    '4': '· · · · -',
+    '5': '· · · · ·',
+    '6': '- · · · ·',
+    '7': '- - · · ·',
+    '8': '- - - · ·',
+    '9': '- - - - ·',
+    '.': '· - · - · -',
+    ',': '- - · · - -',
+    '?': '· · - - · ·'
+}
+
+phrase = input("Insert a message: ")
+phrase = phrase.upper()
+morse_msg = ""
+for n in phrase:
+    if n == " ":
+        morse_msg += "  "
+    else:
+        morse_msg += morse_code[n]
+print(morse_msg)
+
