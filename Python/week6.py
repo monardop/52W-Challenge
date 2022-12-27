f8f3acdfa5cad8a05d6e7874eae43283cde17a25
Pablo Monardo <pablomonardo@gmail.com>
2022-12-26 23:08:06 -0300

    Create week6.py

---
+++ Python/week6.py
@@ -0,0 +1,11 @@
+"""
+ * Reto #6
+ * INVIRTIENDO CADENAS
+ * Dificultad: FÁCIL
+ *
+ * Enunciado: Crea un programa que invierta el orden de una cadena de texto sin usar funciones propias del lenguaje que
+ lo hagan de forma automática.
+ * - Si le pasamos "Hola mundo" nos retornaría "odnum aloH"
+"""
+
+phrase = input("Insert a string: ")
