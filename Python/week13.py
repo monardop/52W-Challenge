92d12835aa2be6b6007c0596f68650dc8930a620
Pablo Monardo <pablomonardo@gmail.com>
2022-12-29 12:18:53 -0300

    Created week13.py

---
+++ Python/week13.py
@@ -0,0 +1,10 @@
+"""
+ * Reto #13
+ * FACTORIAL RECURSIVO
+ * Dificultad: FÁCIL
+ *
+ * Enunciado: Escribe una función que calcule y retorne el factorial de un número dado de forma recursiva.
+"""
+
+
+
