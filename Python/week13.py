e843dafd7565ae5c63093f4a0dc9ffac249c101d
Pablo Monardo <pablomonardo@gmail.com>
2022-12-29 12:32:23 -0300

    Finished w13

--- Python/week13.py
+++ Python/week13.py
@@ -7,4 +7,32 @@
 """
 
 
+def factorial(number: int, fact=1):
+    if number - 1 == 0:
+        return fact
+    return factorial(number - 1, fact * number)
 
+
+def valid_number() -> int:
+    while True:
+        try:
+            number = int(input("Insert a number: "))
+            if number < 0:
+                raise ValueError
+        except ValueError:
+            print("That's not a valid number")
+        else:
+            return number
+
+
+def main():
+    number = valid_number()
+    if number == 0:
+        print("1")
+    else:
+        print(factorial(number))
+    return 0
+
+
+if __name__ == '__main__':
+    main()
