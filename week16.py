7c39c39a062ec05e0beb8a0c98a8320b29d109c4
Pablo Monardo <pablomonardo@gmail.com>
2022-12-30 18:18:10 -0300

    Create week16.py

---
+++ week16.py
@@ -0,0 +1,11 @@
+"""
+ * Reto #16
+ * EN MAYÚSCULA
+ * Dificultad: FÁCIL
+
+ * Enunciado: Crea una función que reciba un String de cualquier tipo y se encargue de
+ * poner en mayúscula la primera letra de cada palabra.
+ * - No se pueden utilizar operaciones del lenguaje que lo resuelvan directamente.
+"""
+
+
