bd1ccddd845730d4a6ba27c49f235dc98996566d
Pablo Monardo <pablomonardo@gmail.com>
2023-01-03 20:46:03 -0300

    Create test_week23.py

---
+++ Python/test_week23.py
@@ -0,0 +1,10 @@
+import unittest
+
+
+class TestResults(unittest.TestCase):
+    def test1(self):
+        self.assertEqual(True, False)  # add assertion here
+
+
+if __name__ == '__main__':
+    unittest.main()
