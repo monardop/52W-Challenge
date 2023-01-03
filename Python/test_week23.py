39321fe6650fa9d96839b0f38c835e81a435cced
Pablo Monardo <pablomonardo@gmail.com>
2023-01-03 20:54:59 -0300

    Update test_week23.py

--- Python/test_week23.py
+++ Python/test_week23.py
@@ -1,9 +1,22 @@
 import unittest
+import week23 as f
 
 
 class TestResults(unittest.TestCase):
     def test1(self):
-        self.assertEqual(True, False)  # add assertion here
+        print("Testing factorization")
+        ans = f.factorization(48)
+        self.assertEqual(ans, [2, 2, 2, 2, 3])
+
+    def test2(self):
+        print("Testing gcd")
+        ans = f.gcd(f.factorization(48), f.factorization(180))
+        self.assertEqual(ans, 12)
+
+    def test3(self):
+        print("Testing lcm")
+        ans = f.lcm(48, 180)
+        self.assertEqual(ans, 720)
 
 
 if __name__ == '__main__':
