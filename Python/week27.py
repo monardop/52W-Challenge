0db6a2d8ecc91073b044563b3a90092f856d655f
Pablo Monardo <pablomonardo@gmail.com>
2023-01-04 10:37:23 -0300

    Create week27.py

---
+++ Python/week27.py
@@ -0,0 +1,10 @@
+"""
+ * Reto #27
+ * VECTORES ORTOGONALES
+ * Dificultad: FÁCIL
+ *
+ * Enunciado: Crea un programa que determine si dos vectores son ortogonales.
+ * - Los dos array deben tener la misma longitud.
+ * - Cada vector se podría representar como un array. Ejemplo: [1, -2]
+ *
+"""
