ecd15321dcfcec63ef2d1942453fa1534f97aae2
Pablo Monardo <pablomonardo@gmail.com>
2023-01-03 21:01:21 -0300

    Update test_week23.py

--- Python/test_week23.py
+++ Python/test_week23.py
@@ -3,18 +3,15 @@ import week23 as f
 
 
 class TestResults(unittest.TestCase):
-    def test1(self):
-        print("Testing factorization")
+    def test_factorization(self):
         ans = f.factorization(48)
         self.assertEqual(ans, [2, 2, 2, 2, 3])
 
-    def test2(self):
-        print("Testing gcd")
+    def test_gcd(self):
         ans = f.gcd(f.factorization(48), f.factorization(180))
         self.assertEqual(ans, 12)
 
-    def test3(self):
-        print("Testing lcm")
+    def test_lcm(self):
         ans = f.lcm(48, 180)
         self.assertEqual(ans, 720)
 
