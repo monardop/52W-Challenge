b1735674f5b7c240b30acadddc83a02ab79d8584
Pablo Monardo <pablomonardo@gmail.com>
2023-01-03 22:04:42 -0300

    Update week26.py

--- Python/week26.py
+++ Python/week26.py
@@ -7,3 +7,22 @@
  * - Indicaremos el tamaño del lado y si la figura a dibujar es una u otra.
  * - EXTRA: ¿Eres capaz de dibujar más figuras?
 """
+
+
+def draw_square(length: int) -> None:
+    icon = '*'
+    for n in range(0, length+1):
+        if n in [0, length]:
+            print(icon*length)
+        else:
+            print(f"{icon}{' '*(length-2)}{icon}")
+
+
+def draw_triangle(length: int):
+    icon = '*'
+    for n in range(1, length+1):
+        print(icon*n)
+
+
+draw_square(int(input("Insert length: ")))
+draw_triangle(int(input("Insert length: ")))
