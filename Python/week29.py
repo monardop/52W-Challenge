217b23bb7d2d47295a8a34ee313c6d172ffe442f
Pablo Monardo <pablomonardo@gmail.com>
2023-01-04 14:02:36 -0300

    Create week29.py

---
+++ Python/week29.py
@@ -0,0 +1,10 @@
+"""
+ * Challenge #29
+ * ORDER THE LIST
+ * Difficulty: EASY
+ *
+ * Statement: Create a function that sorts and returns an array of numbers.
+ * - The function will receive a list (for example [2, 4, 6, 8, 9]) and an additional parameter.
+ * "Asc" or "Desc" to indicate whether to sort from smallest to largest or from largest to smallest.
+ * You cannot use the language's own functions to solve it automatically.
+"""
