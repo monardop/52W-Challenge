41e9a2603515827ddef6920e50bc5288b0159712
Pablo Monardo <pablomonardo@gmail.com>
2023-01-03 21:45:08 -0300

    Create week26.py

---
+++ Python/week26.py
@@ -0,0 +1,9 @@
+"""
+* Reto #26
+ * CUADRADO Y TRIÁNGULO 2D
+ * Dificultad: FÁCIL
+ *
+ * Enunciado: Crea un programa que dibuje un cuadrado o un triángulo con asteriscos "*".
+ * - Indicaremos el tamaño del lado y si la figura a dibujar es una u otra.
+ * - EXTRA: ¿Eres capaz de dibujar más figuras?
+"""
