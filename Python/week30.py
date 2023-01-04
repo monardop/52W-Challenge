1e2abb2443de76d961fdbb64141daa2ae5b60deb
Pablo Monardo <pablomonardo@gmail.com>
2023-01-04 14:40:29 -0300

    Create week30.py

---
+++ Python/week30.py
@@ -0,0 +1,11 @@
+"""
+ * Challenge #30
+ * WORD FRAME
+
+ * Difficulty: EASY
+ *
+ * Statement: Create a function that receives a text and displays each word on a line,
+ forming a rectangular frame of asterisks.
+ * a rectangular frame of asterisks.
+"""
+
