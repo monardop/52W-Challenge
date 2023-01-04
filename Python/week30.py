36e63a41b11a3f3f1ddbd1282db1ff7fa514b3f2
Pablo Monardo <pablomonardo@gmail.com>
2023-01-04 15:31:28 -0300

    Update week30.py

--- Python/week30.py
+++ Python/week30.py
@@ -9,3 +9,23 @@
  * a rectangular frame of asterisks.
 """
 
+
+def get_length(elements: list) -> int:
+    max_len = 0
+    for element in elements:
+        if len(element) > max_len:
+            max_len = len(element)
+    return max_len
+
+
+def draw_square(phrase: str) -> None:
+    icon = '*'
+    aux_phrase = phrase.split()
+    length = get_length(aux_phrase)
+
+    for n, word in zip(range(length), aux_phrase):
+        if n in [0, len(aux_phrase)]:
+            print(icon*length)
+        else:
+            print(f"{icon}{' '*(length-2)}{icon}")
+
