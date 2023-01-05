17c8336902b0e8b808d11c37ad771c4b769a0b8e
Pablo Monardo <pablomonardo@gmail.com>
2023-01-05 17:20:07 -0300

    Update week33.py

--- Python/week33.py
+++ Python/week33.py
@@ -15,7 +15,33 @@
 
 ELEMENT = ('Wood', 'Fire', 'Earth', 'Metal', 'Water')
 ANIMAL = ('Rat', 'Ox', 'Tiger', 'Rabbit', 'Dragon', 'Snake', 'Horse', 'Sheep', 'Monkey', 'Rooster', 'Dog', 'Pig')
-YEAR = 1924
 
-def get_sexagenarian(year: int):
 
+def get_sexagenarian(year: int) -> int:
+    base_year = 1924
+    sexagenarian = 1
+    while True:
+        if base_year <= year <= base_year + 60:
+            return base_year, sexagenarian
+        else:
+            base_year += 60
+            sexagenarian += 1
+
+
+def get_animal_element(year: int) -> tuple:
+    animal: str = ANIMAL[year % len(ANIMAL)]
+    element: str = ELEMENT[year % len(ELEMENT)]
+    return animal, element
+
+
+def main():
+    year = int(input("Year: "))
+    sexagenarian = get_sexagenarian(year)
+    pair = get_animal_element(abs(year - sexagenarian[0]))
+    print(f"The year {year} is {pair[0]} - {pair[1]}")
+    print(f"It belong to the {sexagenarian[1]} sexagenarian, that starts in {sexagenarian[0]} "
+          f"and ends on {sexagenarian[0] + 60} ")
+
+
+if __name__ == '__main__':
+    main()
