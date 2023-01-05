31133d57012ae4b61f980f32667f6b047cb8f8f3
Pablo Monardo <pablomonardo@gmail.com>
2023-01-05 17:29:27 -0300

    Create week34.py

---
+++ Python/week34.py
@@ -0,0 +1,11 @@
+"""
+ * Challenge #34
+ * THE MISSING NUMBERS
+ * Difficulty: EASY
+ *
+ * Statement: Given a sorted array of integers with no repeats, create a function that computes
+ * and returns all the missing ones between the largest and the smallest.
+ * - Throws an error if the input array is not correct.
+
+"""
+
