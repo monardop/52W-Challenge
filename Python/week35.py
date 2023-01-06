bad794da007e0710b9f5d78f9ee939b10d0bc402
Pablo Monardo <pablomonardo@gmail.com>
2023-01-06 12:21:14 -0300

    Update week35.py

--- Python/week35.py
+++ Python/week35.py
@@ -1,26 +1,15 @@
-"""
- * Reto #35
- * BATALLA POKÉMON
- * Dificultad: MEDIA
- *
- * Enunciado: Crea un programa que calcule el daño de un ataque durante una batalla Pokémon.
- * - La fórmula será la siguiente: daño = 50 * (ataque / defensa) * efectividad
- * - Efectividad: x2 (súper efectivo), x1 (neutral), x0.5 (no es muy efectivo)
- * - Sólo hay 4 tipos de Pokémon: Agua, Fuego, Planta y Eléctrico (buscar su efectividad)
- * - El programa recibe los siguientes parámetros:
- *  - Tipo del Pokémon atacante.
- *  - Tipo del Pokémon defensor.
- *  - Ataque: Entre 1 y 100.
- *  - Defensa: Entre 1 y 100.
-"""
 from abc import ABC, abstractmethod
+from random import randint
 
 
 class Pokemon(ABC):
     def __init__(self, name, p_class):
         self.name = name
         self.p_class = p_class
-        self. life = 100
+        self. life = randint(100, 1000)
+        self.power = randint(1, 100)
+        self.defense = randint(1, 100)
+        self.is_defending = False
 
     @abstractmethod
     def attack(self, victim):
@@ -29,17 +18,40 @@ class Pokemon(ABC):
     @abstractmethod
     def defend(self, attacker):
         pass
+
+    @abstractmethod
+    def counter(self, atk_def: bool, pkm_type: str) -> float:
+        """
+        Check for the combination, so it can return the booster
+        :param atk_def: True for attack, False for defend
+        :param pkm_type: pokemon type
+        :return: The booster for the defense or attack
+        """
 
 
 class FirePokemon(Pokemon):
     def __init__(self, name):
         super().__init__(name, "Fire")
 
-    def attack(self, victim):
-        pass
+    def attack(self, victim: Pokemon):
+        effectiveness = self.counter(True, victim.p_class)
+        if victim.is_defending:
+            pass
+        else:
+            return 50 * (self.power / self.defense) * effectiveness
 
     def defend(self, attacker):
-        pass
+        self.is_defending = True
+
+    def counter(self, atk_def: bool, pkm_type: str) -> float:
+        pokemon_type = ('Fire', 'Water', 'Plant', 'Electricity')
+        enemy: int = pokemon_type.index(pkm_type)
+        attack = (0.5, 0.5, 2, 1)
+        defense = (0.5, 2, 0.5, 1)
+        if atk_def:
+            return attack[enemy]
+        else:
+            return defense[enemy]
 
 
 class WaterPokemon(Pokemon):
@@ -52,6 +64,16 @@ class WaterPokemon(Pokemon):
     def defend(self, attacker):
         pass
 
+    def counter(self, atk_def: bool, pkm_type: str) -> float:
+        pokemon_type = ('Fire', 'Water', 'Plant', 'Electricity')
+        enemy: int = pokemon_type.index(pkm_type)
+        attack = (2, 0.5, 0.5, 1)
+        defense = (0.5, 0.5, 2, 2)
+        if atk_def:
+            return attack[enemy]
+        else:
+            return defense[enemy]
+
 
 class PlantPokemon(Pokemon):
     def __init__(self, name):
@@ -63,6 +85,16 @@ class PlantPokemon(Pokemon):
     def defend(self, attacker):
         pass
 
+    def counter(self, atk_def: bool, pkm_type: str) -> float:
+        pokemon_type = ('Fire', 'Water', 'Plant', 'Electricity')
+        enemy: int = pokemon_type.index(pkm_type)
+        attack = (2, 0.5, 0.5, 0.5)
+        defense = (0.5, 2, 0.5, 1)
+        if atk_def:
+            return attack[enemy]
+        else:
+            return defense[enemy]
+
 
 class ElectricPokemon(Pokemon):
     def __init__(self, name):
@@ -72,4 +104,14 @@ class ElectricPokemon(Pokemon):
         pass
 
     def defend(self, attacker):
-        pass
+        self.is_defending = True
+
+    def counter(self, atk_def: bool, pkm_type: str) -> float:
+        pokemon_type = ('Fire', 'Water', 'Plant', 'Electricity')
+        enemy: int = pokemon_type.index(pkm_type)
+        attack = (1, 2, 0.5, 0.5)
+        defense = (1, 1, 1, 0.5)
+        if atk_def:
+            return attack[enemy]
+        else:
+            return defense[enemy]
