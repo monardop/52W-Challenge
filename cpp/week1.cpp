df67dddbdb07c88e97fedeb56d0a9cbb5daf626a
Pablo Monardo <pablomonardo@gmail.com>
2023-01-06 17:26:38 -0300

    Update week1.cpp

--- cpp/week1.cpp
+++ cpp/week1.cpp
@@ -4,15 +4,15 @@ int main()
 {
     for (int i = 1 ; i < 100; i++)
     {
-        if (i%3 == 0 && i%5 == 0)
+        if (i % 3 == 0 && i % 5 == 0)
         {
             std::cout << "FizzBuzz" << std::endl;
         }
         
-        else if(i%3 == 0){
+        else if(i % 3 == 0){
             std::cout << "Fizz"<< std::endl;
         }
-        else if (i%5 == 0)
+        else if (i % 5 == 0)
         {
             std::cout << "Buzz"<< std::endl;
         }
