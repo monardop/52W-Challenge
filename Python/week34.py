d1621792b507bab64e592a9f76faf8f5f071daff
Pablo Monardo <pablomonardo@gmail.com>
2023-01-06 11:09:18 -0300

    Update week34.py

--- Python/week34.py
+++ Python/week34.py
@@ -9,3 +9,57 @@
 
 """
 
+
+def set_array(*args):
+    try:
+        if valid_array(args):
+            return args
+        else:
+            raise ValueError
+    except ValueError:
+        exit(1)
+
+
+def valid_array(numbers: list) -> bool:
+    if len(numbers) != len(set(numbers)):
+        print("There are repeated values")
+        return False
+    for n in numbers:
+        if n is str:
+            print(f"{n} is not a number")
+            return False
+    return True
+
+
+def decorator(function):
+
+    def wrapper():
+        lowest, highest = function()
+        i = 0
+        between_numbers = [n for n in range(lowest, highest + 1)]
+        for n in between_numbers:
+            if n not in array:
+                print(n, end="\t")
+            else:
+                i += 1
+        if i == len(between_numbers):
+            print("All if them are on your array")
+
+    return wrapper
+
+
+@decorator
+def get_min_max() -> tuple:
+    lowest: int = array[0]
+    highest: int = array[0]
+    for n in array:
+        if lowest > n:
+            lowest = n
+        if highest < n:
+            highest = n
+    return lowest, highest
+
+
+if __name__ == '__main__':
+    array = set_array(1, 2, 7, 43, 3, 55)
+    get_min_max()
