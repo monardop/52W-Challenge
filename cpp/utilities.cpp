c87934a9cedfb3667b537bab9eb547b0af7c4ea9
Pablo Monardo <pablomonardo@gmail.com>
2023-01-09 12:10:17 -0300

    Create utilities.cpp

---
+++ cpp/utilities.cpp
@@ -0,0 +1,29 @@
+/*
+This module will have the function of storing validations and other types of functions that are necessary and that do not come by default in the standard library.
+*/
+
+#include <iostream>
+
+int validInput(int least, int greatest)
+{
+    int input;
+    do
+    {
+      std::cout << "Insert a number between" << least << " and " << greatest << ": ";
+      std::cin >> input;  
+    } while (least < input < greatest);
+    
+    return input;
+}
+
+int positiveNumber()
+{
+    int input;
+    do
+    {
+        std::cout << "Insert a positive number: ";
+        std::cin >> input;
+    } while (input > 0);
+    
+    return input;
+}
