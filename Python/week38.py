4634e362043dd1ae9caccf67a927ce720b4faf34
Pablo Monardo <pablomonardo@gmail.com>
2023-01-09 16:30:18 -0300

    Update week38.py

--- Python/week38.py
+++ Python/week38.py
@@ -1,3 +1,44 @@
 """
 An application that transforms binary numbers to decimals
 """
+
+
+def get_number() -> str:
+    while True:
+        try:
+            number = input("Insert a binary number: ")
+            for bit in number:
+                if bit not in ['1', '0']:
+                    raise ValueError
+        except ValueError:
+            print("Wrong input, try again.")
+        else:
+            return number
+
+
+def get_decimal(binary: str, sign_bit=False) -> int:
+    bytes_used = 0
+    is_negative = False
+    if sign_bit:
+        bytes_used = len(binary) - 2
+        if binary[0] == '1':
+            is_negative = True
+            binary.replace("1", "", 1)
+        else:
+            is_negative = False
+    else:
+        bytes_used = len(binary) - 1
+
+    decimal_number = 0
+    for n in binary:
+        decimal_number += int(n) * (2 ** bytes_used)
+        bytes_used -= 1
+    if is_negative:
+        return decimal_number * -1
+    else:
+        return decimal_number
+
+
+print(get_decimal("1101101", True))
+
+# TODO: The negative function is not doing right
