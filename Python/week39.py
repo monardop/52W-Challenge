53ccb451ded6686f946af1c30f90911e74e3da65
Pablo Monardo <pablomonardo@gmail.com>
2023-01-10 18:57:52 -0300

    Update week39.py

--- Python/week39.py
+++ Python/week39.py
@@ -1,20 +1,48 @@
 """
 Quick Sort algorithm
 """
+from random import randint
+from time import time
 
 
-def quicksort(unsorted_list):
+def partition(array, start, end):
+    pivot = array[start]
+    low = start + 1
+    high = end
 
-    if len(unsorted_list) > 2:
-        v = unsorted_list[0] + unsorted_list[len(unsorted_list) - 1] + unsorted_list[len(unsorted_list) - 1 / 2] / 3
-    elif len(unsorted_list) == 2:
-        v = unsorted_list[0] + unsorted_list[1] / 2
-    else:
-        v = unsorted_list[0]
+    while True:
+        while low <= high and array[high] >= pivot:
+            high = high - 1
 
-    return quicksort(filter((lambda y: y < v), unsorted_list)) \
-        + [v] \
-        + quicksort(filter((lambda y: y >= v), unsorted_list))
+        while low <= high and array[low] <= pivot:
+            low = low + 1
+
+        if low <= high:
+            array[low], array[high] = array[high], array[low]
+        else:
+            break
+    array[start], array[high] = array[high], array[start]
+
+    return high
 
 
-print(quicksort([9,4,2,3,1,5,6]))
+def quick_sort(array, start, end):
+    if start >= end:
+        return
+
+    new_partition = partition(array, start, end)
+    quick_sort(array, start, new_partition - 1)
+    quick_sort(array, new_partition + 1, end)
+
+
+lista = []
+start_time = time()
+for n in range(1000):
+    lista.append(randint(0, 1000))
+quick_sort(lista, 0, len(lista) - 1)
+print(lista)
+elapsed_time = time() - start_time
+print(f"Elapsed time: {elapsed_time}")
+
+
+
