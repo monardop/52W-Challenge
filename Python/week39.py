a21e65467a583194dcf7ba0eee71155a11d627a4
Pablo Monardo <pablomonardo@gmail.com>
2023-01-10 11:24:21 -0300

    Update week39.py

--- Python/week39.py
+++ Python/week39.py
@@ -1,3 +1,21 @@
 """
 Quick Sort algorithm
 """
+
+
+class Node:
+    def __init__(self, father_node):
+        self.father = father_node
+        self.left_child
+        self.right_child
+
+    def get_mid_value(self):
+        prom = sum(self.father) / len(self.father)
+
+    def set_children(self):
+        pass
+
+
+
+
+
