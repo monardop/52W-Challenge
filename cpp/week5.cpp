08cacc795517a4e461dc3c4fbbc0b6a85121e334
Pablo Monardo <pablomonardo@gmail.com>
2023-01-12 20:27:22 -0300

    Create week5.cpp

---
+++ cpp/week5.cpp
@@ -0,0 +1,24 @@
+/*
+A program that, based on the sides entered, returns the type of figure and its area.
+*/
+
+#include <iostream>
+
+float triangle();
+float square();
+float rectangle();
+
+int main()
+{
+    return 0;
+}
+
+float triangle() 
+{
+
+}
+
+float square(float edge)
+{
+    return (edge * edge);
+}
