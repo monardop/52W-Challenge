0548488398cfad37ce75ec3ca307802bc3920a53
Pablo Monardo <pablomonardo@gmail.com>
2023-01-13 10:25:11 -0300

    Update week5.cpp

--- cpp/week5.cpp
+++ cpp/week5.cpp
@@ -1,5 +1,6 @@
 /*
-A program that, based on the sides entered, returns the type of figure and its area.
+A program that, based on the sides entered 
+:return: the type of figure and its area.
 */
 
 #include <iostream>
@@ -8,17 +9,51 @@ float triangle();
 float square();
 float rectangle();
 
+float (*figure[3])(void) = {triangle, square, rectangle};
+
 int main()
 {
+    int selection;
+    float value;
+
+    std::cout << "Main menu:\n" << "0- Triangle\n" << "1- Square\n" << "2- Rectangle\n";
+    std::cin >> selection; 
+    value = figure[selection]();
+    std::cout << "The area is: " << value << "u2";
+
     return 0;
 }
 
 float triangle() 
 {
+    float base, height;
 
+    std::cout << "Set the base: ";
+    std::cin >> base;
+    std::cout << "Set the height: "; 
+    std::cin >> height;
+
+    return (base * height)/2;
 }
 
-float square(float edge)
+float square()
 {
-    return (edge * edge);
+    float side;
+
+    std::cout << "Set the sides lenght: ";
+    std::cin >> side;
+
+    return (side * side);
+}
+
+float rectangle()
+{
+    float width, height;
+
+    std::cout << "Set the width: ";
+    std::cin >> width;
+    std::cout << "Set the height: "; 
+    std::cin >> height;
+
+    return (width * height);
 }
