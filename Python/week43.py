a57ac144f6f0d5a383fc11709c7300e32c296f86
Pablo Monardo <pablomonardo@gmail.com>
2023-01-16 19:52:31 -0300

    Finished

---
+++ Python/week43.py
@@ -0,0 +1,18 @@
+"""
+ * Enunciado: Crea una función que retorne el número total de bumeranes de
+ * un array de números enteros e imprima cada uno de ellos.
+"""
+from random import randint
+
+list_of_numbers = []
+for n in range(100):
+    list_of_numbers.append(randint(1, 9))
+print(list_of_numbers)
+print("Boomerangs found")
+for n in range(97):
+    if list_of_numbers[n] == list_of_numbers[n+2] != list_of_numbers[n+1]:
+        print(f"[{list_of_numbers[n]}, {list_of_numbers[n+1]}, {list_of_numbers[n+2]}]")
+
+
+
+
