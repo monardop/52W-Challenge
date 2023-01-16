39281af966af9821e75d8a4b53ee2fa1a3939909
Pablo Monardo <pablomonardo@gmail.com>
2023-01-16 20:52:41 -0300

    Update week44.py

--- Python/week44.py
+++ Python/week44.py
@@ -15,4 +15,9 @@ for n in blocks:
     water += max_height - n
 print(f"{water}lts of water")
 
-
+for n in range(max_height + 1, 0):
+    for stage in blocks:
+        if stage >= n:
+            print("T")
+        else:
+            print(':')
