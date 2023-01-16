4362606939a46ed5cf7372bc0bcf7bc09760b06d
Pablo Monardo <pablomonardo@gmail.com>
2023-01-16 19:53:32 -0300

    Created a new challenge

--- Python/week43.py
+++ Python/week43.py
@@ -12,7 +12,3 @@ print("Boomerangs found")
 for n in range(97):
     if list_of_numbers[n] == list_of_numbers[n+2] != list_of_numbers[n+1]:
         print(f"[{list_of_numbers[n]}, {list_of_numbers[n+1]}, {list_of_numbers[n+2]}]")
-
-
-
-
